d5401b6f744361290c9673cecc9d640c85e00521
jsharief <jalaludeen.h@gmail.com>
2020-07-21 07:55:07 -0400

    added comment

--- express/controllers/cartController.js
+++ express/controllers/cartController.js
@@ -2,6 +2,7 @@ const orderManager = require('../manager/orderManager');
 exports.addToCart = (req, res, next) => {
     console.log('Here is in the cart controller.... ');
     let order = orderManager.addToCart(req, res);
+    //This returns the json response
     res.status(200).json({order:order});
 };
   
