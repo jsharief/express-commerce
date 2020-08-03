e2cfccf1552046c4dc0f13a2ef1beb1a9fbb1a88
jsharief <jalaludeen.h@gmail.com>
2020-08-02 22:31:41 -0400

    removed the datasource url

--- express/dao/dataSource.js
+++ express/dao/dataSource.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 let mongoServiceUrl =
-  "mongodb+srv://sharief:Login123$@cluster0.mzumy.mongodb.net/Test?retryWrites=true&w=majority&useUnifiedTopology=true";
+  
 
 exports.dataSource = ()=>{
 
